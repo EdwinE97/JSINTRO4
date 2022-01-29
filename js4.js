669805aa6da234d81f7063e5f29e8a5893a1b9a0
EdwinE97 <94918451+EdwinE97@users.noreply.github.com>
2022-01-29 18:35:11 -0500

    Completed medium

--- js4.js
+++ js4.js
@@ -9,7 +9,7 @@
 
 function findAverage(array) {
     let avg = 0;
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let num = array[i];
         avg = num;
     }
@@ -17,5 +17,53 @@ function findAverage(array) {
     return avg;
 };
 
-findAverage([2,9,40]);
+console.log(findAverage([2, 9, 40]));
+
+// MEDIUM: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+// You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+// You may assume no duplicate exists in the array.
+
+// Hint:  Use a function. Use the built in method .indexOf( ) and/or  for loops. Review your lesson on Arrays to aid in working through this problem. 
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+
+// Output: 4
+
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+
+// Output: -1.
+
+nums = [4,5,6,7,0,1,2]
+
+function targetValue(x){
+    var index = nums.indexOf(x)
+     console.log(index) 
+     if (index === undefined){
+         console.log(-1)
+     }
+     
+    }
+     targetValue(7);
+     targetValue(5);
+     targetValue(60);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
