2b5ee53347349deead02318a99dc57c6970c2367
EdwinE97 <94918451+EdwinE97@users.noreply.github.com>
2022-02-01 14:57:22 -0500

    Finished Hard

--- js4.js
+++ js4.js
@@ -54,9 +54,17 @@ function targetValue(x){
      targetValue(60);
 
 
+//      HARD: Create two buttons, one that when clicked turns the background of the body red. The other, when clicked, turns the body's background color white. Each of the background colors should be defined as class styles and when the buttons are pushed the body's classList is updated to include the correct class for the background. 
+
+// Create a single function that takes an input className and updates the body's classList to ONLY include that className
+
+// *Note: Check Canvas for the starter code needed for this exercise. The starter code will be located in a folder named hard_js. 
 
 
 
+function colorChange(color){
+    document.body.style.backgroundColor = color;
+}
 
 
 
